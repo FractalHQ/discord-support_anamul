d2e0fe3f0ac8ea1ba5f29b7c7d1e8b30830e2877
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-09-06 23:34:18 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import hMoment from './moment-hijri';
+</script>
+
+<h1>{hMoment().format('iYYYY/iM/iD')}</h1>
+
+<h2>hello from {browser ? 'browser' : 'server'}</h2>
